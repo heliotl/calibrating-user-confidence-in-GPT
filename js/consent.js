80ff3cfede5ef7d579d9262605e87fd93253a006
Helio Tejeda <htejeda@uci.edu>
2023-10-24 11:21:55 -0700

    Added consent page (html and js) files. Consent page has been updated to reflect experiment compensation (). TODO: write an experiment summary.

---
+++ js/consent.js
@@ -0,0 +1,217 @@
+/*
+consent.js
+
+    Author      :   Helio Tejeda
+    University  :   University of California, Irvine
+    Lab         :   Modeling and Decision-Making Lab (MADLAB)
+
+Consent Page METADATA file.
+
+This file should contain static experimental metadata such as:
+    - Experiment Summary
+    - Experiment Estimated Time
+    - Compensation
+    -
+    -
+*/
+
+/******************************************************************************
+    METADATA
+
+        All metadata variables that are relevant to the consent page.
+******************************************************************************/
+
+/*
+    Experiment Summary
+
+        You want your summary to be descriptive but short.
+
+        DEFAULT:
+            None! This needs to be modified by you!
+*/
+var EXPERIMENT_SUMMARY = `
+    TODO
+`;
+
+/*
+    Time Commitment
+
+        How long will your experiment take (in minutes)?
+
+        Be sure to be as accurate as possible and feel free
+        to include a range (40-50 minutes).
+
+        DEFAULT:
+            30 minutes
+*/
+var TIME_COMMITMENT = "30 minutes";
+
+/*
+    Benefits and Risks
+
+        List any benefits and risks that may arise from participating
+        in your study. If there are multiple, feel free to make a list.
+
+        DEFAULT:
+            There are no direct benefits from your participation in this study.
+*/
+var BENEFITS_AND_RISKS = `
+    There are no direct benefits from your participation in this study.
+`;
+
+/*
+    Eligibility Requirements
+
+        List all eligibility requirements for your study. This includes the
+        criteria you use to filter participants while using Prolific or AMT.
+
+        DEFAULT:
+            1)  United States citizen
+            2)  18 years or older
+            3)  English speaker
+            4)  HTML-5 compatible web browser (chrome, edge, safari, or firefox)
+*/
+var ELIGIBILITY_REQUIREMENTS = `
+    <span>1)</span>     <span class="pl-2">United States citizen/resident</span>
+    <br />
+    <span>2)</span>     <span class="pl-2">18 years or older</span>
+    <br />
+    <span>3)</span>     <span class="pl-2">English speaker</span>
+    <br />
+    <span>4)</span>     <span class="pl-2">HTML-5 compatible web browser (chrome, edge, safari, or firefox)</span>
+`;
+
+/*
+    Compensation
+
+        How much will your participants be getting paid?
+
+        DEFAULT:
+            $5.00
+*/
+var COMPENSATION = "$6.00";
+
+/*
+    Recruitment Service
+
+        Specify the recruitment service that you are using!
+
+        DEFAULT:
+            Amazon Mechanical Turk
+*/
+var RECRUITMENT_SERVICE = "Amazon Mechanical Turk";
+
+/*
+    Comprehention Quiz
+
+        Specify how many questions will be in the comprehention quiz
+        and how many a participant needs to correctly answer in order
+        to participate in the study.
+
+        DEFAULT:
+            4/5
+*/
+var COMPREHENTION_QUIZ = "4/5";
+
+/*
+    Lead Researcher
+
+        This will be you! Be sure to put your name down and email
+        so that participants from your study can contact you if
+        needed.
+
+        DEFAULT:
+            None...Add your name!
+*/
+var LEAD_RESEARCHER = "Aakriti Kumar";
+var LEAD_RESEARCHER_EMAIL   = "aakritk@uci.edu";
+
+
+/******************************************************************************
+    FUNCTIONALITY
+
+        All functions that will be used for the consent page.
+******************************************************************************/
+function disableSubmit() {
+    /*
+        Disable the submit button.
+
+        Submit button should be disabled by default and only enabled once
+        a participant has consented to the study.
+    */
+    if (!document.getElementById("terms").checked) {
+        $("#submit").disabled = true;
+    }
+};
+
+function activateButton(element) {
+    /*
+        Activate the submit button.
+
+        Activates and deactivates the submit button based on a user checking
+        or unchecking the consent box.
+    */
+    if (element.checked) {
+        document.getElementById("submit").disabled = false;
+    }
+    else {
+        document.getElementById("submit").disabled = true;
+    }
+};
+
+function submitConsent() {
+    /*
+        Consent has been submitted and we can move onto the next page.
+
+        Hide all consent page content and activate (show) instruction
+        page content.
+    */
+    // Hide Consent
+    $("#consent-header").attr("hidden", true);
+    $("#consent-main-content").attr("hidden", true);
+    // Show Instructions
+    $("#instructions-header").attr("hidden", false);
+    $("#instructions-main-content").attr("hidden", false);
+};
+
+
+/******************************************************************************
+    RUN ON PAGE LOAD
+
+        Run the following functions as soon as the page is loaded. This will
+        render the consent.html page appropriately.
+******************************************************************************/
+$(document).ready(function (){
+    /*
+        Insert METADATA into page appropriately :)
+    */
+
+    //      Experiment Summary
+    $('#experiment-summary').html(EXPERIMENT_SUMMARY);
+
+    //      Time Commitment
+    $('#time-commitment').text(TIME_COMMITMENT);
+
+    //      Benefits and Risks
+    $('#benefits-and-risks').html(BENEFITS_AND_RISKS);
+
+    //      Eligibility Requirements
+    $('#eligibility-requirements').html(ELIGIBILITY_REQUIREMENTS);
+
+    //      Compensation
+    $('#compensation').text(COMPENSATION);
+    
+    //      Recruitment Service
+    $('#recruitment-service').text(RECRUITMENT_SERVICE);
+
+    //      Comprehension Quiz
+    $('#comprehension-quiz').text(COMPREHENTION_QUIZ);
+
+    //      Lead Researcher Information
+    $('#lead-researcher').text(LEAD_RESEARCHER);
+    $('#lead-researcher-email').text(LEAD_RESEARCHER_EMAIL);
+    $('#lead-researcher-email').attr(
+        "href",
+        "mailto:" + LEAD_RESEARCHER_EMAIL + "?Subject=Noisy%20Image%20Classification"
+    );
+});
