19e502463ae06816b36d5e46b08824ebd6cc92c2
Helio Tejeda <htejeda@uci.edu>
2023-11-07 19:09:33 -0800

    Completed Survey portion of experiment. All functions work. Just need to add work on storing values to firebase.

--- js/survey.js
+++ js/survey.js
@@ -1,6 +1,102 @@
-var checkboxes = {
+/*
+survey.js
 
+    Author      :   Helio Tejeda
+    University  :   University of California, Irvine
+    Lab         :   Modeling and Decision-Making Lab (MADLAB)
+
+Survey Page JS file (metadata and functionality).
+
+This file should contain all variables and functions
+to conduct the post-experiment survey.
+*/
+
+
+/******************************************************************************
+    VARIABLES
+
+        All metadata variables that are relevant to the survey page.
+******************************************************************************/
+//      Turn ON/OFF Debug Mode
+var DEBUG_SURVEY                    = true;
+
+//      Survey Information
+var TOPIC_ABILITY_DICT = {
+    "Anatomy"                       : null,
+    "Global Facts"                  : null,
+    "High School Chemistry"         : null,
+    "High School Macroeconomics"    : null,
+    "High School Physics"           : null,
+    "High School World Histroy"     : null,
+    "Machine Learning"              : null,
+    "Philosophy"                    : null,
+    "Professional Psychology"       : null,
+    "Public Relations"              : null
+}
+var TOPICS_RANKED                   = 0
+
+
+/******************************************************************************
+    FUNCTIONALITY
+
+        All functions that will be used for the survey page.
+******************************************************************************/
+/*
+    Function to control Radio Button Selection
+*/
+function likertTopicAbility() {
+    /*
+        Radio Button Selection Contoller.
+
+        Only one likert option can be selected for each topic.
+        Keep count of how many topics have been ranked. Once all topics
+        have been ranked, then the submit button can become enabled.
+    */
+    // Retrieve the current topic that was ranked
+    let topic_currently_ranked = $(this).attr("name");
+
+    // Determine is that topic has been ranked before or not
+    if (TOPIC_ABILITY_DICT[topic_currently_ranked] == null) {
+        // If the topic hasn't bee ranked before, increment counter
+        TOPICS_RANKED++;
+    }
+
+    // Set selection variable
+    TOPIC_ABILITY_DICT[topic_currently_ranked] = Number($(this).val());
+
+    if (TOPICS_RANKED == 10) {
+        // Enable "Submit" button
+        $('#survey-complete-button').prop('disabled', false);
+    }
+    
+
+    if (DEBUG_SURVEY) {
+        console.log(
+            "Radio Button Selected\n:",
+            "    Topic :", topic_currently_ranked,
+            "    Value :", TOPIC_ABILITY_DICT[topic_currently_ranked]
+        );
+        console.log(
+            $(this).attr("name")
+        );
+    }
 };
+
+function completeExperiment() {
+    /*
+        When submit button is clicked (after ranking), experiment is done.
+
+        This will submit the final rankings and then load the
+        "Experiment Complete" page.
+    */
+    // Hide Instructions
+    $("#exp-survey-header").attr("hidden", true);
+    $("#survey-main-content").attr("hidden", true);
+    // Show Comprehension Quiz
+    $("#exp-complete-header").attr("hidden", false);
+    $("#task-complete").attr("hidden", false);
+};
+
 
 /******************************************************************************
     RUN ON PAGE LOAD
@@ -9,14 +105,9 @@ var checkboxes = {
         render the consent.html page appropriately.
 ******************************************************************************/
 $(document).ready(function (){
-    $('input:radio[name="legend"]').mouseover(function() {
-        console.log("Testing");
-        if(!(this.id in checkboxes))
-          checkboxes[this.id] = this.checked;
-    });
-    $('input:radio[name="legend"]').click(function() {
-        if(!(this.id in checkboxes))
-          checkboxes[this.id] = this.checked;
-        this.checked = checkboxes[this.id];
-    });
+    //  Handle Likert Selection for ALL Topics
+    $('.likert-topic li input').click(likertTopicAbility);
+
+    //  Handle Submitting Survey
+    $('#survey-complete-button').click(completeExperiment);
 });
